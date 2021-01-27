dfc8f28e8ea9708e431e289db180395e28d7a4ee
prajjwal w3dev <prajjwal@w3dev.email>
2021-01-28 01:21:05 +0530

    basic framework

---
+++ app/src/main/java/com/sstTechnologies/smartOt/fragment/signInFrag.java
@@ -0,0 +1,24 @@
+package com.sstTechnologies.smartOt.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.sstTechnologies.smartOt.R;
+
+
+public class signInFrag extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View root = inflater.inflate(R.layout.fragment_sign_in, container, false);
+        return root;
+    }
+}
---
+++ app/src/main/java/com/sstTechnologies/smartOt/fragment/signUpFrag.java
@@ -0,0 +1,25 @@
+package com.sstTechnologies.smartOt.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.sstTechnologies.smartOt.R;
+
+public class signUpFrag extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View root = inflater.inflate(R.layout.fragment_sign_up, container, false);
+
+
+        return root;
+    }
+}
