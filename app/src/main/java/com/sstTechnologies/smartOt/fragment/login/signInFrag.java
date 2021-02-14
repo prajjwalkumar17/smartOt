2036f29eddb7e93d2f9536524b2d651237da8ad4
PrAjJwAl KuMaR <talk2prajjwal@gmail.com>
2021-02-14 15:33:27 +0530

    basic fram

--- app/src/main/java/com/sstTechnologies/smartOt/fragment/login/signInFrag.java
+++ app/src/main/java/com/sstTechnologies/smartOt/fragment/login/signInFrag.java
@@ -17,6 +17,7 @@ public class signInFrag extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
+
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
 
