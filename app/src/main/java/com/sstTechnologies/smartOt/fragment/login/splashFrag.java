b405d4df30d41e2012a5b4d5cba71321dc9e61c0
prajjwal w3dev <prajjwal@w3dev.email>
2021-02-27 02:06:16 +0530

    hjh

--- app/src/main/java/com/sstTechnologies/smartOt/fragment/login/splashFrag.java
+++ app/src/main/java/com/sstTechnologies/smartOt/fragment/login/splashFrag.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.sstTechnologies.smartOt.R;
 
@@ -18,7 +19,7 @@ public class splashFrag extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View root = inflater.inflate(R.layout.fragment_splash, container, false);
-
+        Toast.makeText(getContext(), "jafhfkj", Toast.LENGTH_SHORT).show();
 
         return root;
     }
