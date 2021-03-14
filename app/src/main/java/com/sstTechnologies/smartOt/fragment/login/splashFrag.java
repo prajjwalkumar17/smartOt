3646fc90a82431019502b006a4ebdd601e90cc78
prajjwal w3dev <prajjwal@w3dev.email>
2021-03-15 01:12:03 +0530

    added a new toast1

--- app/src/main/java/com/sstTechnologies/smartOt/fragment/login/splashFrag.java
+++ app/src/main/java/com/sstTechnologies/smartOt/fragment/login/splashFrag.java
@@ -32,6 +32,7 @@ public class splashFrag extends Fragment {
         Toast.makeText(getContext(), "jafhfkj", Toast.LENGTH_SHORT).show();
         Toast.makeText(getContext(), "jafhfkj", Toast.LENGTH_SHORT).show();
         Toast.makeText(getContext(), "jafhfkj", Toast.LENGTH_SHORT).show();
+        Toast.makeText(getContext(), "jafhfkj", Toast.LENGTH_SHORT).show();
 
         return root;
     }
