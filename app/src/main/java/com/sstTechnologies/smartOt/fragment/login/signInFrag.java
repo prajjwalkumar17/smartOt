7776280b64bce1f2ddea7d8109ea0ddd316c1fc2
Pk <talk2prajjwal@gmail.com>
2021-03-25 23:11:54 +0530

    added a new toast1

--- app/src/main/java/com/sstTechnologies/smartOt/fragment/login/signInFrag.java
+++ app/src/main/java/com/sstTechnologies/smartOt/fragment/login/signInFrag.java
@@ -39,6 +39,7 @@ public class signInFrag extends Fragment {
         Toast.makeText(getContext(), "heloom,sxkko", Toast.LENGTH_SHORT).show();
         Toast.makeText(getContext(), "heloom,sxkko", Toast.LENGTH_SHORT).show();
         Toast.makeText(getContext(), "heloom,sxkko", Toast.LENGTH_SHORT).show();
+        Toast.makeText(getContext(), "heloom,sxkko", Toast.LENGTH_SHORT).show();
 
         Toast.makeText(getContext(), "helooo", Toast.LENGTH_SHORT).show();
 
